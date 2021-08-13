6ada0d4b7ebf5cbc1947c72ce615dc973f75bfe3
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-08-13 10:57:00 +0200

    Update StateTest1.java

--- aufgabe3/src/test/java/StateTest1.java
+++ aufgabe3/src/test/java/StateTest1.java
@@ -7,21 +7,19 @@ class StateTest1 {
 
 	@Test
 	void test() {
-		SbSimple1.start();
-		assertEquals(SbSimple1.getStateName(), "S1");
-		SbSimple1.transition("b");
-		assertEquals(SbSimple1.getStateName(), "S2");
-		SbSimple1.transition("d");
-		assertEquals(SbSimple1.getStateName(), "S3");
-		SbSimple1.transition("d");
-		assertEquals(SbSimple1.getStateName(), "S4");
-		SbSimple1.transition("a");
-		assertEquals(SbSimple1.getStateName(), "Final");
+		SbSample2.start();
+		assertEquals(SbSample2.getStateName(), "S1");
+		SbSample2.transition("c");
+		assertEquals(SbSample2.getStateName(), "S2");
+		SbSample2.transition("d");
+		assertEquals(SbSample2.getStateName(), "S6");
+		SbSample2.transition("a");
+		assertEquals(SbSample2.getStateName(), "Final");
 	}
 
 	@Test
 	void transitionCoverage1(){
-		SbSimple1.start();
+		SbSample2.start();
 		assertEquals(SbSimple1.getStateName(), "S1");
 		SbSimple1.transition("b");
 		assertEquals(SbSimple1.getStateName(), "S2");
